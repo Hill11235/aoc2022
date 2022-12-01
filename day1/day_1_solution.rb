69662f4e8980b707dd3b8c2cc2eca5cbfbac91b9
Hill11235 <ethan.hill.05@gmail.com>
2022-12-01 11:52:56 +0000

    Formatting day 1 answer

--- day1/day_1_solution.rb
+++ day1/day_1_solution.rb
@@ -12,4 +12,5 @@ File.readlines(file).each do |line|
 end
 
 tot = totals.sort()
-puts (tot[-1] + tot[-2] + tot[-3])
+puts "Question 1: max calories", tot[-1]
+puts "Question 2: top three max calories", (tot[-1] + tot[-2] + tot[-3])
