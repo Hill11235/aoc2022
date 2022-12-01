1bdbdef6ff0ab2dabc375b58c52b148c35ef38d5
Hill11235 <ethan.hill.05@gmail.com>
2022-12-01 13:48:52 +0000

    Adding missing logic

--- day1/day_1_solution.rb
+++ day1/day_1_solution.rb
@@ -11,6 +11,10 @@ File.readlines(file).each do |line|
   end
 end
 
+if running_total > 0
+  totals.append(running_total)
+end
+
 tot = totals.sort()
 puts "Question 1: max calories", tot[-1]
 puts "Question 2: top three max calories", (tot[-1] + tot[-2] + tot[-3])
