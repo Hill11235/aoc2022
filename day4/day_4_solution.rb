aee3f4181ea30e648017882d537e0c73e803d379
Hill11235 <ethan.hill.05@gmail.com>
2022-12-09 17:28:56 +0000

    Initial txt files and solution file

---
+++ day4/day_4_solution.rb
@@ -0,0 +1,17 @@
+class day4
+
+    def initialize(fileName)
+        @fileName = fileName
+    end
+
+    def question1
+    end
+
+    def question2
+    end
+
+end
+
+dataFile = "test.txt"
+sol = day4.new(dataFile)
+puts sol.question1
