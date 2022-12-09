8f86b35aa69ef093b7f8e319cd06343e7cb0d138
Hill11235 <ethan.hill.05@gmail.com>
2022-12-09 19:50:37 +0000

    Initial files added for day6

---
+++ day6/day_6_solution.rb
@@ -0,0 +1,29 @@
+class Day6
+
+    def initialize(fileName)
+        @fileName = fileName
+    end
+
+    def parseFile
+        File.readlines(@fileName).each do |line|
+            parseLine(line)
+        end
+    end
+
+    def parseLine(line)
+
+    end
+
+    def question1
+
+    end
+
+    def question2
+
+    end
+end
+
+fileName = "test.txt"
+instance = Day6.new(fileName)
+instance.question1
+instance.question2
