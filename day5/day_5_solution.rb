14279c5e1cada082a1882048da68fd09d054f99c
Hill11235 <ethan.hill.05@gmail.com>
2022-12-23 14:23:38 +0000

    Day 5 Q1 solution

--- day5/day_5_solution.rb
+++ day5/day_5_solution.rb
@@ -2,6 +2,7 @@ class Day5
 
     def initialize(fileName)
         @fileName = fileName
+        @stacks = []
     end
 
     def parseFile
@@ -11,11 +12,57 @@ class Day5
     end
 
     def parseLine(line)
+        if @stacks.length == 0
+            createStacks(line)
+        end
+        if line.include?("[")
+            addToStacks(line)
+        elsif line.include?("move")
+            moveItems(line)
+        end
+    end
 
+    def createStacks(line)
+        numStacks = line.length / 4
+        numStacks.times do
+            @stacks.append([])
+        end
+    end
+
+    def addToStacks(line)
+        i = 1
+        chars = line.split('')
+        
+        while i < line.length
+            if chars[i] != " "
+                index = (i - 1) / 4
+                @stacks[index].append(chars[i])
+            end
+            i += 4
+        end
+    end
+
+    def moveItems(line)
+        words = line.split
+        shiftFreight(words[1].to_i, words[3].to_i, words[5].to_i)
+    end
+
+    def shiftFreight(howMany, from, to)
+        howMany.times do
+            cargo = @stacks[from - 1].shift
+            @stacks[to - 1].unshift(cargo)
+        end
+    end
+
+    def printTopLine()
+        @stacks.each { |x|
+            print x[0]
+        }
     end
 
     def question1
-
+        parseFile
+        printTopLine
     end
 
     def question2
@@ -23,7 +70,7 @@ class Day5
     end
 end
 
-fileName = "test.txt"
+fileName = "real.txt"
 instance = Day5.new(fileName)
 instance.question1
-instance.question2
+#instance.question2
